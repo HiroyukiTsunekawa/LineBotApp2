d9e8a0fd012ce13462082a3630c8ef453e59ca10
Hiroyuki Tsunekawa 恒川 <h.tsunekawa@haitex.hayatele.co.jp>
2017-02-28 19:54:02 -0600

    LineBot用にWebに記載どおりに修正

--- app.js
+++ app.js
@@ -15,6 +15,41 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+// ----------ここから----------
+// ----------ここから2----------
+var request = require('request');
+
+app.post('/api', function(req, res) {
+  var options = {
+    method: 'POST',
+    uri: 'https://api.line.me/v2/bot/message/reply',
+    body: {
+      replyToken: req.body.events[0].replyToken,
+      messages: [{
+        type: "text",
+        text: req.body.events[0].message.text
+      }]
+    },
+    auth: {
+      bearer: 'IZ5WinYpt9+ngEMIo+fl3ue7WwtVJ1B1gsieu4zCtgXGT42xkfhR+BLIZrQXi5Q6y4O8LEuEEiiGTj5SkG3zTqJBQx4jnYZCr80FpaJx1oozAiERg9HOAKv/g/ac4mAeDjuJnhE2uQHmWCIc9VtrUAdB04t89/1O/w1cDnyilFU=' // ここは自分のtokenに書き換える
+    },
+    json: true
+  };
+  request(options, function(err, res, body) {
+    console.log(JSON.stringify(res));
+  });
+  res.send('OK');
+});
+// --------ここまで追加2--------
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+// --------ここまで追加--------
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
